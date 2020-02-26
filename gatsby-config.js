20d1cd1b764cc6b66f6ad582d9603f6dc96084ff
Jeff Bliss <jeff.a.bliss@gmail.com>
2020-02-26 09:04:07 -0500

    updated based on hello world tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,14 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-module.exports = {
-  /* Your site config here */
-}
+ module.exports = {
+   pathPrefix: "/learning_gatsby"
+   plugins: [
+     {
+       resolve: `gatsby-plugin-typography`,
+       options: {
+         pathToConfigModule: `src/utils/typography`,
+       },
+     },
+   ],
+ }
